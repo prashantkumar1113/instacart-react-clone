8850737885512396345df65cab78be0a497c43f4
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-04-16 13:34:03 -0500

    Created the create-react-app and added some demo components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Herobar from "./components/Herobar";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* 
+        <navbar>
+        <herobar>
+        <qrbar>
+        <chooseStores>
+        <cardbar>
+      */}
+            <Navbar />
+            <Herobar />
+        </div>
+    );
 }
 
 export default App;
