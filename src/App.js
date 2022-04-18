05aea08199bb2ce3c40158faf0cfaa79c3a097c5
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-04-17 21:02:32 -0500

    Added the MUI Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import Navbar from "./components/Navbar";
 import Herobar from "./components/Herobar";
 import "./App.css";
 
 function App() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                // Purple and green play nicely together.
+                main: "#ffffff",
+            },
+            secondary: {
+                // This is green.A700 as hex.
+                main: "#08AC0A",
+            },
+        },
+    });
+
     return (
         <div className="App">
             {/* 
@@ -13,8 +27,10 @@ function App() {
         <chooseStores>
         <cardbar>
       */}
-            <Navbar />
-            <Herobar />
+            <ThemeProvider theme={theme}>
+                <Navbar />
+                <Herobar />
+            </ThemeProvider>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,64 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/MenuRounded";
 
 export default function Navbar() {
     return (
-        <div>
-            <h3>Navbar</h3>
-        </div>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar
+                position="static"
+                sx={{
+                    boxShadow: "none",
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 0}}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box sx={{flexGrow: 1}}>
+                        <img
+                            src="./images/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg"
+                            alt="instacart logo"
+                        />
+                    </Box>
+                    {/* <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        News
+                    </Typography> */}
+                    <Button
+                        color="inherit"
+                        sx={{
+                            textTransform: "none",
+                            boxShadow: "none",
+                            fontSize: "1.1em",
+                        }}
+                    >
+                        Log in
+                    </Button>
+                    <Button
+                        color="secondary"
+                        variant="contained"
+                        sx={{
+                            textTransform: "none",
+                            borderRadius: "15px",
+                            boxShadow: "none",
+                            fontSize: "1.1em",
+                        }}
+                    >
+                        Sign Up
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
